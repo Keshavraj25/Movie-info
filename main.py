fef3f2b9985215dd5944d4d62e9b6d3f8e10d266
Keshavraj25 <rs9504858@gmail.com>
2025-08-12 15:24:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8347005060:AAHf11nTICnku70OKIcX8OccXr8DlhKa17s")
 API_ID = int(os.getenv("API_ID", "26954495"))
 API_HASH = os.getenv("API_HASH", "2061c55207cfee4f106ff0dc331fe3d9")
-TMDB_API = os.getenv("TMDB_API", "0da8b26f661ce60b48bb5f2876e13c74")
+TMDB_API = os.getenv("TMDB_API", "6abcb6bb99fb77f33c37016a28866ed2")
 IMDB_API = os.getenv("IMDB_API", "https://imdb-api-lux.wemedia360.workers.dev/")
 
 # Telegram Client
